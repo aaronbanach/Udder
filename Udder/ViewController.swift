33c56c7c86a3e17b7dec4c34bb512fbaad11a01f
Aaron Banach <aaronbanach@hotmail.com>
2018-06-24 12:56:53 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Aaron Banach on 6/24/18.
+//  Copyright © 2018 Aaron Banach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
